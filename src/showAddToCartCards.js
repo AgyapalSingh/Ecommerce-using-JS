e69b2548f83fecc69d90b74ea4f77124eccb09f6
Agyapal Singh <agyapalsinghaujla@gmail.com>
2024-12-04 12:12:33 +0530

    Quanity Inc/ Dec and Remove product form Cart functionality

--- src/showAddToCartCards.js
+++ src/showAddToCartCards.js
@@ -1,5 +1,8 @@
 import products from "../api/products.json";
 import { getCartProductFromLS } from "../src/getCartProducts";
+import { fetchQuantityFromCartLS } from "./fetchQuantityFromCartLS";
+import { incrementDecrement } from "./incrementDecrement";
+import { removeProdFromCart } from "./removeProdFromCart";
 
 let cartProducts = getCartProductFromLS();
 let filterProducts = products.filter((currProd) => {
@@ -16,11 +19,28 @@ const showCartProduct = () => {
     const { category, id, image, name, stock, price } = currProd;
 
     let productClone = document.importNode(templateContainer.content, true);
+
+    const lSActualData = fetchQuantityFromCartLS(id, price);
+
     productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
     productClone.querySelector(".category").textContent = category;
     productClone.querySelector(".productName").textContent = name;
     productClone.querySelector(".productImage").src = image;
 
+    productClone.querySelector(".productQuantity").textContent =
+      lSActualData.quantity;
+    productClone.querySelector(".productPrice").textContent =
+      lSActualData.price;
+
+    productClone
+      .querySelector(".stockElement")  
+      .addEventListener("click", (event) => {
+        incrementDecrement(event, id, stock, price);
+      });
+
+    productClone
+      .querySelector(".remove-to-cart-button")
+      .addEventListener("click", () => removeProdFromCart(id));
 
     cartElement.appendChild(productClone);
   });
